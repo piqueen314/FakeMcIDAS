cd956facca5c2a35b3eb85544dde5ecb48a09d03
Cece Hedrick <cecehedrick@gmail.com>
2018-04-10 17:15:44 -0600

    add a simple fake app to the project

---
+++ fakeMcIDAS/src/main/java/edu/fake/ssec/fakeHydra/fakeHydraClass1.java
@@ -0,0 +1,27 @@
+package edu.fake.ssec.fakeHydra;
+
+public class fakeHydraClass1 {
+
+    public void printer (String message){
+        System.out.print(message);
+    }
+    public int adder (int num1, int num2){
+        return num1 + num2;
+    }
+    public double [] columnSums(double[][] data){
+        int numRows = data.length;
+        int numCols = data[0].length;
+        double [] resultSums = new double [numCols];
+
+        for(int col=0; col < numCols; col++){
+
+            for (int row= 0; row < numRows; row++) {
+                resultSums[col] += data[row][col];
+                System.out.println(resultSums[col]);
+            }
+        }
+        return resultSums;
+
+
+    }
+}
